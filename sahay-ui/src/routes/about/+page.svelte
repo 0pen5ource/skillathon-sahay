722edaf6e6dced873b399f4e0220debcf5eefc31
Dileep Bapat <todo>
2023-02-24 16:00:22 +0530

    Adding UI project for the BAP

---
+++ sahay-ui/src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+	<title>About Sahay</title>
+	<meta name="description" content="Sahay is mentorship connect application" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Sahay is mentorship connect possibility</h1>
+
+	<p>
+		Sahay leverages the power of <a href="https://github.com/beckn/DSEP-Specification">DSEP API</a> to discover and connect best available resources for skill development and guidance.
+	</p>
+
+	<p>
+		If you are looking for a mentor, you can search for mentors based on your skillset and location. You can also search for mentors based on your interests and hobbies.
+	</p>
+
+	<p>
+		If you are a mentor, you can register yourself as a mentor and help others in their skill development journey.
+	</p>
+</div>
---
+++ sahay-ui/src/routes/signup/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+    let name = "";
+    let email = "";
+    let phone = "";
+    let telegramHandle = "";
+    let errorMessage = "";
+async function postForm() {
+    try{
+        const response = await fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                phone,
+                telegramHandle
+            })
+        });
+        if (response.ok) {
+            alert('success');
+        } else {
+            throw new Error('Something went wrong');
+        }
+    } catch (error) {
+        errorMessage = error.message;
+    }
+}
+</script>
+
+<h1>Register</h1>
+{#if errorMessage}
+    <p style="color:chocolate">{errorMessage}</p>
+{/if}
+<table>
+    <tr>
+        <td><label for="name">Name</label></td>
+        <td><input name="name" id="name" bind:value={name}></td></tr>
+    <tr><td><label for="email">Email</label></td>
+        <td><input name="email" id="email" bind:value={email}></td></tr>
+    <tr><td><label for="telegram">Telegram</label></td>
+    <td><input name="telegram" id="telegram" bind:value={telegramHandle}></td>
+    </tr>
+    <tr><td><label for="phone">Phone</label></td>
+    <td><input name="phone" id="phone" bind:value={phone}></td></tr>
+    <tr><td><button on:click|preventDefault={postForm}>Sign Up</button></td></tr>
+</table>
