d4d75d9005824ebb2abbc2c5803458c053ad2a5c
Dileep Bapat <todo>
2023-02-25 10:50:54 +0530

    changes for login

--- sahay-ui/src/routes/signup/+page.svelte
+++ sahay-ui/src/routes/signup/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+    let page = "register"
     let name = "";
     let email = "";
     let phone = "";
     let telegramHandle = "";
     let errorMessage = "";
+    let otp = "";
+    let sessionToken = "";
 async function postForm() {
     try{
         const response = await fetch('/api/register', {
@@ -15,11 +18,41 @@ async function postForm() {
                 name,
                 email,
                 phone,
-                telegramHandle
+                telegram: telegramHandle
             })
         });
         if (response.ok) {
             alert('success');
+            const respBody = await response.json();
+            sessionToken = respBody.sessionToken;
+            page = "verify"
+        } else {
+            throw new Error('Something went wrong');
+        }
+    } catch (error) {
+        errorMessage = error.message;
+    }
+}
+async function verifyOTP() {
+    try{
+        const response = await fetch('/api/verify', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                otp,
+                sessionToken: sessionToken
+            })
+        });
+        if (response.ok) {
+            const body = await response.json()
+            if (body.status === 'success') {
+                alert('success');
+                window.location.replace('/')
+            } else {
+                throw new Error('Something went wrong');
+            }
         } else {
             throw new Error('Something went wrong');
         }
@@ -33,16 +66,25 @@ async function postForm() {
 {#if errorMessage}
     <p style="color:chocolate">{errorMessage}</p>
 {/if}
-<table>
-    <tr>
-        <td><label for="name">Name</label></td>
-        <td><input name="name" id="name" bind:value={name}></td></tr>
-    <tr><td><label for="email">Email</label></td>
-        <td><input name="email" id="email" bind:value={email}></td></tr>
-    <tr><td><label for="telegram">Telegram</label></td>
-    <td><input name="telegram" id="telegram" bind:value={telegramHandle}></td>
-    </tr>
-    <tr><td><label for="phone">Phone</label></td>
-    <td><input name="phone" id="phone" bind:value={phone}></td></tr>
-    <tr><td><button on:click|preventDefault={postForm}>Sign Up</button></td></tr>
-</table>
+{#if page === "register"}
+    <table>
+        <tr>
+            <td><label for="name">Name</label></td>
+            <td><input name="name" id="name" bind:value={name}></td></tr>
+        <tr><td><label for="email">Email</label></td>
+            <td><input name="email" id="email" bind:value={email}></td></tr>
+        <tr><td><label for="telegram">Telegram</label></td>
+            <td><input name="telegram" id="telegram" bind:value={telegramHandle}></td>
+        </tr>
+        <tr><td><label for="phone">Phone</label></td>
+            <td><input name="phone" id="phone" bind:value={phone}></td></tr>
+        <tr><td><button on:click|preventDefault={postForm}>Sign Up</button></td></tr>
+    </table>
+{/if}
+{#if page === "verify"}
+    <p> Enter OTP below</p>
+    <div>
+    <input type="text" name="otp" id="otp"  bind:value={otp}>
+    <button on:click={verifyOTP}>Verify</button>
+    </div>
+{/if}
