be9ec7ca9c8c4f3cccd5d7a15b1f79be7510ebb1
Dileep Bapat <todo>
2023-02-26 16:13:38 +0530

    Mentor search

--- sahay-ui/src/routes/+page.svelte
+++ sahay-ui/src/routes/+page.svelte
@@ -1,5 +1,26 @@
 <script>
 	import gurukul from '$lib/images/gurukul.png';
+	let token = 'sahay';
+	export const load = (async ({ cookies }) => {
+		token = cookies.split(';').find(row => row.trim().startsWith('token='));
+	});
+	let searchText = ''
+	let log = ''
+	const search = async () => {
+		console.log(searchText);
+		const resp = await fetch('/api/search', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				sessionTitle: searchText
+			})
+		})
+		const data = await resp.json()
+		log += JSON.stringify(data, null, 2)
+	};
+
 </script>
 
 <svelte:head>
@@ -8,17 +29,27 @@
 </svelte:head>
 
 <section>
-	<a href="/signup">Enroll</a>
-	<h1>
-		<span class="welcome">
-			<picture>
-<!--				<source srcset={welcome} type="image/webp" />-->
-				<img src={gurukul} alt="Welcome" />
-			</picture>
-		</span>
+	{#if token}
+		<label for="search">Search</label>
+		<input id="search" bind:value={searchText} placeholder="Language tutor">
+		<button on:click={search}>Search</button>
+
+		<br/>
+
+		<textarea bind:value={log} />
+	{:else}
+		<a href="/signup">Enroll</a>
+		<h1>
+			<span class="welcome">
+				<picture>
+	<!--				<source srcset={welcome} type="image/webp" />-->
+					<img src={gurukul} alt="Welcome" />
+				</picture>
+			</span>
 
 
-	</h1>
+		</h1>
+	{/if}
 
 
 
